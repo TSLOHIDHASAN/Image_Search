63fc8352d31600d43edd2f8139b5f78ace73dba4
TSLOHIDHASAN <lohidhasan2020@gmail.com>
2020-07-16 05:32:25 +0530

    Updates

---
+++ precache-manifest.5e1875a8fdbf90f0596f9be6bc73cbfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de67dd3deefca56caefe9af6a34344fd",
+    "url": "/Image_Search/index.html"
+  },
+  {
+    "revision": "8d23b72ebf508dad409d",
+    "url": "/Image_Search/static/css/main.1feb54ba.chunk.css"
+  },
+  {
+    "revision": "e33d29e69669432efcf3",
+    "url": "/Image_Search/static/js/2.58488f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Image_Search/static/js/2.58488f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d23b72ebf508dad409d",
+    "url": "/Image_Search/static/js/main.fe69a076.chunk.js"
+  },
+  {
+    "revision": "8bf9489b16fbc3f5066b",
+    "url": "/Image_Search/static/js/runtime-main.a9b4f6b0.js"
+  }
+]);
